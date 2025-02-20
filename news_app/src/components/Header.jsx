1d9d04837aed75d82e7b26aaff631a9d942945d2
YeIn kim <2076070@ewhain.net>
2025-02-20 22:32:43 +0900

    feat: 마이페이지 드롭다운 추가

--- news_app/src/components/Header.jsx
+++ news_app/src/components/Header.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   const [login, setLogin] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header className="header">
       <div className="logo">
         <Link to="/">NEWjeanS</Link>
       </div>
       <nav className="header-btn">
-        <Link to="/mypage">MYPAGE</Link>
         {login && (
           <button className="logout-btn" onClick={() => setLogin(!login)}>
             Logout
@@ -21,6 +21,22 @@ const Header = () => {
             Login
           </Link>
         )}
+        <div class="menu-container">
+          <span onClick={() => setIsOpen((prev) => !prev)}>MY MENU</span>
+          {isOpen && (
+            <ul class="dropdown-menu">
+              <li>
+                <Link to="/mypage">MY PAGE</Link>
+              </li>
+              <li>
+                <Link to="/mypage">관심 주제</Link>
+              </li>
+              <li>
+                <Link to="/mypage">스크랩한 뉴스</Link>
+              </li>
+            </ul>
+          )}
+        </div>
       </nav>
     </header>
   );
