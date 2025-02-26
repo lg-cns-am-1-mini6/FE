e93bc35ec6a48713c31ed48fbbc403075a81fced
YeIn kim <2076070@ewhain.net>
2025-02-26 14:18:30 +0900

    feat: Footer에 광고 추가

--- news_app/src/components/Footer.jsx
+++ news_app/src/components/Footer.jsx
@@ -5,11 +5,24 @@ export default function Footer() {
   return (
     <>
       <footer className="footer-body">
-        <p>footer example</p>
-        <p>footer example</p>
-        <p>footer example</p>
-        <p>footer example</p>
-        <Link to="/admin">DO NOT TOUCH ME !!</Link>
+        <div className="footer-contents">
+          <div className="admin-link">
+            <Link to="/admin">DO NOT TOUCH ME !!</Link>
+          </div>
+          <div>
+            <a
+              href="https://link.coupang.com/a/cgtL9H"
+              target="_blank"
+              referrerpolicy="unsafe-url"
+            >
+              <img
+                src="https://ads-partners.coupang.com/banners/835088?subId=&traceId=V0-301-bae0f72e5e59e45f-I835088&w=728&h=90"
+                alt=""
+              />
+            </a>
+          </div>
+          <br></br>
+        </div>
       </footer>
     </>
   );
